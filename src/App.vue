f4714610e364d0652250e2f847f377dd58a314ae
gingerbeam <152566655+gingerbeam@users.noreply.github.com>
2024-05-19 06:48:20 +0800

    vue files

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
 <template>
   <div id="app">
-    <h1>Message: {{ message }}</h1>
-    <button @click="fetchMessage1">Get Message 1</button>
-    <button @click="fetchMessage2">Get Message 2</button>
+    <my-component></my-component>
   </div>
 </template>
 
 <script>
-import { getMessage1, getMessage2 } from './contractUtils';
+import MyComponent from './components/ConnectWallet.vue';
 
 export default {
   name: 'App',
-  data() {
-    return {
-      message: ''
-    };
-  },
-  methods: {
-    async fetchMessage1() {
-      try {
-        this.message = await getMessage1();
-      } catch (error) {
-        console.error('Error fetching message 1:', error);
-      }
-    },
-    async fetchMessage2() {
-      try {
-        this.message = await getMessage2();
-      } catch (error) {
-        console.error('Error fetching message 2:', error);
-      }
-    }
+  components: {
+    MyComponent
   }
-};
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* CSS 样式 */
 </style>
