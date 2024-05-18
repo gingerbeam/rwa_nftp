cb78642653f29049c7bc908fee47ceb4e96a62a3
gingerbeam <gingerbeam@outlook.com>
2024-05-18 21:38:18 +0000

    very primary version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Message: {{ message }}</h1>
+    <button @click="fetchMessage1">Get Message 1</button>
+    <button @click="fetchMessage2">Get Message 2</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { getMessage1, getMessage2 } from './contractUtils';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: ''
+    };
+  },
+  methods: {
+    async fetchMessage1() {
+      try {
+        this.message = await getMessage1();
+      } catch (error) {
+        console.error('Error fetching message 1:', error);
+      }
+    },
+    async fetchMessage2() {
+      try {
+        this.message = await getMessage2();
+      } catch (error) {
+        console.error('Error fetching message 2:', error);
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
