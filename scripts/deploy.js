7f749615dd9410ecdeb456823cc45be39c91ac7f
gingerbeam <gingerbeam@outlook.com>
2024-05-18 21:28:13 +0000

    deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+// scripts/deploy.js
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const MyContract = await ethers.getContractFactory("MyContract");
+  const myContract = await MyContract.deploy("Hello, Scroll!");
+  console.log("Contract deployed to address:", myContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
